c1ebacd623ce6a6d0b553f92ad978bfeb83132e2
Casey Dentinger <cdentinger@wikimedia.org>
2016-12-28 14:01:48 -0700

    update libs
    Change-Id: I7a7cd9f2f797511409496d116d34c5b85a094ea1

--- composer/autoload_namespaces.php
+++ composer/autoload_namespaces.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'PHPQueue' => array($vendorDir . '/coderkungfu/php-queue/src'),
     'FuseSource' => array($vendorDir . '/fusesource/stomp-php/src'),
     'Clio' => array($vendorDir . '/clio/clio/src'),
