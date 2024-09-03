365524139c47736bbfea115aba3671e0abae614d
Jack Gleeson <jgleeson@wikimedia.org>
2024-09-03 21:22:20 +0100

    Update Gravy SDK to v0.21.4
    Change-Id: I8d820d4c9cc77e1f94111f0a2b888fe947c95b7f

--- composer/autoload_real.php
+++ composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit442d5bd04c536bc08fb965344071e9ce
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit442d5bd04c536bc08fb965344071e9ce::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire442d5bd04c536bc08fb965344071e9ce($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit442d5bd04c536bc08fb965344071e9ce::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire442d5bd04c536bc08fb965344071e9ce($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
